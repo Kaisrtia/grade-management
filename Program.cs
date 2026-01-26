5e576c5cb374511390911c143863a4a8d3dcfb91
HT_Kaisrtia <nqhtrung304@gmail.com>
2026-01-27 02:23:28 +0700

    add .sln file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using GradeManagement.Model;
+using GradeManagement.Service;
 internal class Program {
   private static void Main (string[] args) {
     Admin admin = new Admin ();
     System.Console.WriteLine (admin.role);
     Course math = new Course ("1", "Math");
     System.Console.WriteLine ($"Course ID: {math.id}, Course Name: {math.name}");
+    
   }
 }
